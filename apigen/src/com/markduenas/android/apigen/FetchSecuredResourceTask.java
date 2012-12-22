9be7393c9766bda41eee7827b2aafe5eb8223ba9
Mark <markduenas@gmail.com>
2012-12-21 22:00:26 -0700

    Initial commit

---
+++ apigen/src/com/markduenas/android/apigen/FetchSecuredResourceTask.java
@@ -0,0 +1,26 @@
+package com.markduenas.android.apigen;
+
+import java.util.Collections;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class FetchSecuredResourceTask extends AsyncTask<String, Void, String>
+{
+
+	 @Override
+	 protected void onPreExecute()
+	 {
+	 }
+
+	@Override
+	protected String doInBackground(String... urls)
+	{
+		return "";
+	}
+
+	@Override
+	protected void onPostExecute(String result)
+	{
+		
+	}
+}
