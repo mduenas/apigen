8c7fbc49e3f9998f3aa452296e3ae7e8713f84c1
Mark Duenas <markduenas@gmail.com>
2012-12-24 19:16:00 -0700

    other files

---
+++ apigen/gen/com/markduenas/android/apigenactivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.markduenas.android.apigenactivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
